30c17f2a22b92258ea4ea81a4f1f664f0a7e6623
lukaszjastrzebski86-beep <lukasz.jastrzebski86@gmail.com>
2026-04-13 23:56:27 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 20 }}>
+      <h1>AI Platform działa 🚀</h1>
+      <p>To jest Twój pierwszy projekt AI</p>
+    </main>
+  );
+}
